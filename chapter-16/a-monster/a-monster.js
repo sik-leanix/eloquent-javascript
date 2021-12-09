dc7ccfb875eca829f295cd71ca85889ff9a0ae03
Sidney Klass <sidney.klass@leanix.net>
2021-12-09 16:08:52 +0100

    Improve the code like konstantin suggested

--- chapter-16/a-monster/a-monster.js
+++ chapter-16/a-monster/a-monster.js
@@ -1,54 +1,52 @@
 import { DOMDisplay, Vec, levelChars, runLevel, Level, State } from "../lib/game.js";
 
 class Monster {
-    constructor(pos) {
-        this.pos = pos;
+  constructor(pos) {
+    this.pos = pos;
+    this.speed = 3;
+  }
+
+  get type() { return "monster"; }
+
+  static create(pos) {
+    return new Monster(pos.plus(new Vec(0, -1)));
+  }
+
+  update(time, state) {
+    const player = state.player;
+    const playerPosX = player.pos.x;
+    let newMonsterPos;
+    const monsterPos = this.pos;
+    if (this.pos.x < playerPosX) {
+      newMonsterPos = new Vec(monsterPos.x + (time * this.speed), monsterPos.y);
+    } else {
+      newMonsterPos = new Vec(monsterPos.x - (time * this.speed), monsterPos.y);
     }
-
-    get type() { return "monster"; }
-
-    static create(pos) {
-      return new Monster(pos.plus(new Vec(0, -1)));
+    if (state.level.touches(newMonsterPos, this.size, "wall")) {
+      return new Monster(new Vec(this.pos.x, this.pos.y));
+    } else {
+      return new Monster(newMonsterPos)
     }
+  }
 
-    update(time, state) {
-        const player = state.player;
-      const playerPosX = player.pos.x;
-      let newPos;
-      let speed = 0.05;
-      if (this.pos.x < playerPosX) {
-          newPos = new Vec(this.pos.x + speed, this.pos.y);
-      } else {
-          newPos = new Vec(this.pos.x - speed, this.pos.y);
-      }
-      if (state.level.touches(newPos, this.size, "wall")) {
-          return new Monster(new Vec(this.pos.x, this.pos.y));
-      } else {
-          return new Monster(newPos)
-      }
-      
+  collide(state) {
+    const player = state.player;
+    const playerPosY = Math.floor(player.pos.y);
+    if (Math.floor(this.pos.y) - playerPosY === 2) {
+      const status = state.status;
+      const actorsWithoutMonster = state.actors.filter(a => a != this);
+      return new State(state.level, actorsWithoutMonster, status);
+    } else {
+      return new State(state.level, state.actors, "lost");
     }
+  }
+} 
 
-    collide(state) {
-      const player = state.player;
-      const playerPosY = Math.floor(player.pos.y);
-      if (Math.floor(this.pos.y) - playerPosY === 2) {
-        let status = state.status;
-        let filtered = state.actors.filter(a => a != this);
-        if (!filtered.some(a => a.type == "coin")) status = "won";
-        return new State(state.level, filtered, status);
-        
-      } else {
-        return new State(state.level, state.actors, "lost");
-        }
-      }
-  } 
+Monster.prototype.size = new Vec(1.2, 2);
 
-  Monster.prototype.size = new Vec(1.2, 2);
+levelChars["M"] = Monster;
 
-  levelChars["M"] = Monster;
-
-  runLevel(new Level(`
+runLevel(new Level(`
 ..................................
 .################################.
 .#..............................#.
